d914441084bc2a75ebb420fa08bd9ce2b72429dd
Naquiya Asghar <naquiya.asghar@lokal.pk>
2023-08-01 15:57:08 +0500

    Made the footer and toggle field responsive

---
+++ script.js
@@ -0,0 +1,45 @@
+//write this in indexed.html
+
+/* <div class="place-list" id="placeList">
+    </div>
+
+    <!-- Include the external JavaScript file -->
+    <script src="path/to/your/script.js"></script> */
+
+// // Sample data for the places
+// const places = [
+// { icon: 'bi bi-rocket', text: 'OMG!', active: true },
+// { icon: 'bi bi-heart', text: 'Amazing Pools' },
+// { icon: 'bi bi-list', text: 'Trending' },
+// { icon: 'bi bi-rocket', text: 'Tropical' },
+// { icon: 'bi bi-airplane', text: 'Rooms' },
+// { icon: 'bi bi-activity', text: 'Beach' },
+// { icon: 'bi bi-alarm', text: 'Desert' },
+// { icon: 'bi bi-android', text: 'Mansion' },
+// { icon: 'bi bi-arrow-down', text: 'Amazing Views' }
+// ];
+
+// // Function to create and add elements to the list
+// function createPlaceList() {
+// const placeList = document.getElementById('placeList');
+
+// places.forEach(place => {
+// const link = document.createElement('a');
+// link.href = '#';
+// link.className = 'a-link' + (place.active ? ' active-place' : ' place-item');
+
+// const icon = document.createElement('i');
+// icon.className = place.icon + (place.active ? ' active-place-icon' : ' place-icon');
+
+// const text = document.createElement('p');
+// text.className = place.active ? 'active-place-text' : 'place-text';
+// text.textContent = place.text;
+
+// link.appendChild(icon);
+// link.appendChild(text);
+// placeList.appendChild(link);
+// });
+// }
+
+// // Call the function to create the list
+// createPlaceList();
